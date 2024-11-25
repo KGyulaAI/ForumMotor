8f6e3d44ce05360edea1f0b8145d98e749ca217a
Kovács Gyula <kovacs.gyula@burg.lo.hu>
2024-11-25 09:30:52 +0100

    vmi

--- ForumMotor/Data/Migrations/20241125081344_mssql.local_migration_416.Designer.cs
+++ ForumMotor/Data/Migrations/20241125081344_mssql.local_migration_416.Designer.cs
@@ -4,6 +4,7 @@ using ForumMotor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ForumMotor.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241125081344_mssql.local_migration_416")]
+    partial class mssqllocal_migration_416
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
